1ed656c3d28410731aa5a2f11751f83ed2e275fb
Kaytmazov <altair@list.ru>
2018-04-24 17:59:51 +0300

    module5-task2

--- js/util.js
+++ js/util.js
@@ -2,7 +2,6 @@
 
 (function () {
   window.util = {
-    // Получаем случайные данные из массива
     getRandomItemFromArray: function (array) {
       var randomItem = array[Math.floor(Math.random() * array.length)];
       return randomItem;
