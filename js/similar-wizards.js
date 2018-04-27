f9da013de9d1bc564c0679757d1bd68b09f31670
Kaytmazov <altair@list.ru>
2018-04-27 10:04:28 +0300

    Загружает похожие персонажи с сервера и отправляет форму

--- js/similar-wizards.js
+++ js/similar-wizards.js
@@ -2,37 +2,19 @@
 
 (function () {
   var WIZARDS_COUNT = 4;
-  var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+  var LOAD_URL = 'https://js.dump.academy/code-and-magick/data';
 
   var setup = document.querySelector('.setup');
   var similarListElement = setup.querySelector('.setup-similar-list');
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
-  // Генерация массива случайных похожих персонажей
-  var generateWizards = function (wizardsCount) {
-    var wizardsArray = [];
-
-    for (var i = 0; i < wizardsCount; i++) {
-      wizardsArray[i] = {
-        name: window.util.getRandomItemFromArray(WIZARD_NAMES) + ' ' + window.util.getRandomItemFromArray(WIZARD_SURNAMES),
-        coatColor: window.util.getRandomItemFromArray(COAT_COLORS),
-        eyesColor: window.util.getRandomItemFromArray(EYES_COLORS)
-      };
-    }
-
-    return wizardsArray;
-  };
 
   // Создаем элемент
   var makeWizardElement = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
 
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
 
     return wizardElement;
   };
@@ -40,15 +22,17 @@
   // Функция отрисовки элементов
   var renderWizards = function (wizards) {
     var fragment = document.createDocumentFragment();
-    for (var i = 0; i < wizards.length; i++) {
+    for (var i = 0; i < WIZARDS_COUNT; i++) {
       fragment.appendChild(makeWizardElement(wizards[i]));
     }
     return fragment;
   };
 
-  var wizards = generateWizards(WIZARDS_COUNT);
-  similarListElement.append(renderWizards(wizards));
+  var onLoad = function (wizards) {
+    var shaffledWizardsArray = window.util.shuffleArray(wizards);
+    similarListElement.append(renderWizards(shaffledWizardsArray));
+    document.querySelector('.setup-similar').classList.remove('hidden');
+  };
 
-  // Показываем Список похожих персонажей
-  document.querySelector('.setup-similar').classList.remove('hidden');
+  window.backend.load(onLoad, window.util.onRequestError, LOAD_URL);
 })();
--- js/util.js
+++ js/util.js
@@ -5,6 +5,30 @@
     getRandomItemFromArray: function (array) {
       var randomItem = array[Math.floor(Math.random() * array.length)];
       return randomItem;
+    },
+    // Перемешать массив
+    shuffleArray: function (array) {
+      var sheffledArray = [];
+      var clone = array.slice();
+
+      while (clone.length) {
+        var index = Math.floor(Math.random() * clone.length);
+        var element = clone.splice(index, 1)[0];
+        sheffledArray.push(element);
+      }
+
+      return sheffledArray;
+    },
+    onRequestError: function (errorMessage) {
+      var node = document.createElement('div');
+      node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+      node.style.position = 'absolute';
+      node.style.left = 0;
+      node.style.right = 0;
+      node.style.fontSize = '30px';
+
+      node.textContent = errorMessage;
+      document.body.insertAdjacentElement('afterbegin', node);
     }
   };
 })();
