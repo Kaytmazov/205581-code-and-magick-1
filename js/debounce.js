1c05d4a8fbcf3ae2b507b30940f9745914cd6dc9
Kaytmazov <altair@list.ru>
2018-05-03 19:33:22 +0300

    Исправляет debounce

--- js/debounce.js
+++ js/debounce.js
@@ -7,6 +7,7 @@
   window.debounce = function (fun) {
     if (lastTimeout) {
       window.clearTimeout(lastTimeout);
+      lastTimeout = null;
     }
     lastTimeout = window.setTimeout(fun, DEBOUNCE_INTERVAL);
   };
